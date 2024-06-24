133c53e3fd46fd3f949151a7a5e9ebaa50bc1e6a
hzt599880511 <98630269+hzt599880511@users.noreply.github.com>
2024-06-24 16:10:27 +0800

    Update main.js

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ newDiv.className = 'inserted-element';
 newDiv.textContent = 'money! money! money! ';
 const a = new Date('2024-06-19 00:00:00').getTime()
 const b = new Date().getTime();
+console.log(newDiv)
 if (b > a) {
-    document.body.appendChild(newDiv);
+    if(newDiv){
+        document.body.appendChild(newDiv);
+    }
 }
